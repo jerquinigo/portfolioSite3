0392807ad2ef6877c6041194affc6a77dbceccfa
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-08-28 15:20:40 -0400

    initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>home</div>;
+  }
+}
+
+export default Home;
