b72ae1b73c3fd8f2163a026671f9ae9e19e58756
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-08-30 11:34:41 -0400

    modifying the navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,24 +7,37 @@ class NavBar extends Component {
   //   super();
   // }
 
+  componentDidMount() {
+    document.addEventListener("DOMContentLoaded", function() {
+      var elems = document.querySelectorAll(".sidenav");
+      var instances = M.Sidenav.init(elems);
+    });
+  }
+
   displayNavbar = () => {
     return (
       <div>
         <nav className="nav-wrapper indigo">
           <div className="container">
+            <a href="#" class="brand-logo">
+              Logo
+            </a>
             <a href="#" className="sidenav-trigger" data-target="mobile-links">
               <i className="material-icons">menu</i>
             </a>
 
             <ul className="right hide-on-med-and-down">
-              <li>link 1</li>
-              <li>link 2</li>
-              <li>link 3</li>
+              <li>Home</li>
+              <li>Projects</li>
+              <li>Skills</li>
             </ul>
           </div>
         </nav>
 
         <ul className="sidenav" id="mobile-links">
+          <div className="logo-div indigo">
+            <li className="indigo">Jonathan Erquinigo</li>
+          </div>
           <li>link 1</li>
           <li>link 2</li>
           <li>link 3</li>
