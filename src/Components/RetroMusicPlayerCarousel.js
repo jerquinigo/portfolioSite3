7fddbc792fb02e406425a06183989bd5556da9bf
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-09-03 14:17:16 -0400

    retro music player images

--- src/Components/RetroMusicPlayerCarousel.js
+++ src/Components/RetroMusicPlayerCarousel.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-
+import RetroMusicPlayerNirvanna from "../Assets/RetroMusicPlayerCarousel/RetroMusicPlayerNirvanna.png";
+import RetroMusicPlayerRiseAgainst from "../Assets/RetroMusicPlayerCarousel/RetroMusicPlayerRiseAgainst.png";
+import RetroMusicPlayerSum41 from "../Assets/RetroMusicPlayerCarousel/RetroMusicPlayerSum41.png";
+import RetroMusicPlayerPhum from "../Assets/RetroMusicPlayerCarousel/RetroMusicPlayerPhum.png";
 class RetroMusicPlayerCarousel extends Component {
   render() {
     return (
       <Carousel>
         <div>
-          <img src="https://snworksceo.imgix.net/bsd/2af5201b-24a2-44ab-8aaf-c0194f7a82f0.sized-1000x1000.png?w=800" />
+          <img src={RetroMusicPlayerNirvanna} alt="" />
         </div>
         <div>
-          <img src="https://snworksceo.imgix.net/bsd/2af5201b-24a2-44ab-8aaf-c0194f7a82f0.sized-1000x1000.png?w=800" />
+          <img src={RetroMusicPlayerRiseAgainst} alt="" />
         </div>
         <div>
-          <img src="https://snworksceo.imgix.net/bsd/2af5201b-24a2-44ab-8aaf-c0194f7a82f0.sized-1000x1000.png?w=800" />
+          <img src={RetroMusicPlayerSum41} alt="" />
+        </div>
+        <div>
+          <img src={RetroMusicPlayerPhum} alt="" />
         </div>
       </Carousel>
     );
